87bdf8731323314579aaa21ee8e7b5266d7d2683
Rafael Pagels <raffx.320@gmail.com>
2025-12-29 20:42:15 -0300

    Add files via upload

---
+++ 01_basico/media_alunos.c.cpp
@@ -0,0 +1,29 @@
+/* 
+* Programa básico em C
+* Calcula a média do aluno
+* Conteúdo: entrada/saída, variáveis, if/else
+*/
+
+#include <stdio.h>
+#include <locale.h>
+
+float nota1, nota2, media;
+
+int main (){
+	setlocale(LC_ALL, "PORTUGUESE");
+
+	printf ("\nEntre com a primeira nota: ");
+	scanf ("%f", &nota1);
+	printf ("\nEntre com a segunda nota: ");
+	scanf ("%f", &nota2);
+	media = (nota1 + nota2) / 2;
+	printf ("\nA média final é de: %.2f", media);
+	
+	if (media <= 5.9)
+		printf ("\nVocê foi reprovado.");
+	else
+		printf ("\nVocê foi aprovado!");
+		
+	return 0;	
+}
+
