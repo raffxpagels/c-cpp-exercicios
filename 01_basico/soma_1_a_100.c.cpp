da36b543fd3a8d0485293489b585d05ebbb01cfc
Rafael Pagels <raffx.320@gmail.com>
2025-12-29 21:07:26 -0300

    Add files via upload

---
+++ 01_basico/soma_1_a_100.c.cpp
@@ -0,0 +1,24 @@
+/*
+* Programa básico em C
+* Calcula a soma de todos os números de 1 a 100
+* Conteúdo: for, variáveis, acumulador
+*/
+ 
+#include <stdio.h>
+#include <locale.h>
+
+int contador, soma;
+
+int main (){
+	soma = 0;
+	setlocale (LC_ALL, "PORTUGUESE");
+	
+	printf ("A soma dos números de 1 a 100 é de: \n\n");
+	
+		for (contador = 1; contador <= 100; contador++){
+			printf ("%d + %d = %d\n", contador, soma, soma + contador);
+			soma += contador;			
+		}
+	printf ("O resultado final é de %d", soma);
+	return 0;	
+}
